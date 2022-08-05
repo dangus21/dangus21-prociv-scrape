5c8a4d0a9f19edda7918b78824e8e2a52bf35079
Miguel Magalhaes <ext.miguel.magalhaes@farfetch.com>
2022-08-05 23:59:52 +0100

    main: [] back to api folder

--- pages/api/scrape_prociv.ts
+++ pages/api/scrape_prociv.ts
@@ -1,78 +1,81 @@
 import puppeteer from 'puppeteer';
 
 export default async function scrape(req, res) {
-    const browser = await puppeteer.launch({
-        headless: true,
-        args: [
-            '--start-maximized', // you can also use '--start-fullscreen'
-        ],
-    });
+    try {
+        const browser = await puppeteer.launch();
 
-    const page = await browser.newPage();
+        const page = await browser.newPage();
 
-    await page.setViewport({ width: 1920, height: 1080 });
+        await page.setViewport({ width: 1920, height: 1080 });
 
-    await page.goto(
-        'http://www.prociv.pt/pt-pt/SITUACAOOPERACIONAL/Paginas/default.aspx?cID=11'
-    );
+        await page.goto(
+            'http://www.prociv.pt/pt-pt/SITUACAOOPERACIONAL/Paginas/default.aspx?cID=11'
+        );
 
-    await page.waitForFunction(
-        () =>
-            (
-                document.querySelector(
-                    '#listOcorrenciasDetails tbody'
-                ) as HTMLElement
-            ).childElementCount || 0 > 0
-    );
+        await page.waitForFunction(
+            () =>
+                (
+                    document.querySelector(
+                        '#listOcorrenciasDetails tbody'
+                    ) as HTMLElement
+                ).childElementCount || 0 > 0
+        );
 
-    await page.evaluate(() => {
-        const element = document.querySelector('#listOcorrenciasDetails tbody');
-        if (element) {
-            element.scrollIntoView();
-        }
-    });
-
-    const isElementVisible = async (
-        page: puppeteer.Page,
-        cssSelector: string
-    ) => {
-        let visible = true;
-        await page
-            .waitForSelector(cssSelector, { visible: true, timeout: 2000 })
-            .catch(() => {
-                visible = false;
-            });
-        return visible;
-    };
-
-    let loadMoreVisible = await isElementVisible(
-        page,
-        '#listOcorrenciasDetails > table > tfoot > tr > th > span'
-    );
-    while (loadMoreVisible) {
-        await page
-            .click('#listOcorrenciasDetails > table > tfoot > tr > th > span')
-            .catch((e) => {
-                // eslint-disable-next-line no-console
-                console.log(e);
-            });
         await page.evaluate(() => {
             const element = document.querySelector(
-                '#listOcorrenciasDetails > table > tfoot > tr > th > span'
+                '#listOcorrenciasDetails tbody'
             );
             if (element) {
                 element.scrollIntoView();
             }
         });
-        loadMoreVisible = await isElementVisible(
+
+        const isElementVisible = async (
+            page: puppeteer.Page,
+            cssSelector: string
+        ) => {
+            let visible = true;
+            await page
+                .waitForSelector(cssSelector, { visible: true, timeout: 2000 })
+                .catch(() => {
+                    visible = false;
+                });
+            return visible;
+        };
+
+        let loadMoreVisible = await isElementVisible(
             page,
             '#listOcorrenciasDetails > table > tfoot > tr > th > span'
         );
+        while (loadMoreVisible) {
+            await page
+                .click(
+                    '#listOcorrenciasDetails > table > tfoot > tr > th > span'
+                )
+                .catch((e) => {
+                    // eslint-disable-next-line no-console
+                    console.log(e);
+                });
+            await page.evaluate(() => {
+                const element = document.querySelector(
+                    '#listOcorrenciasDetails > table > tfoot > tr > th > span'
+                );
+                if (element) {
+                    element.scrollIntoView();
+                }
+            });
+            loadMoreVisible = await isElementVisible(
+                page,
+                '#listOcorrenciasDetails > table > tfoot > tr > th > span'
+            );
+        }
+
+        const screenshot = await page.screenshot({ encoding: 'base64' });
+        await browser.close();
+
+        res.json({ blob: `data:image/jpeg;base64,${screenshot}` });
+    } catch (error) {
+        // eslint-disable-next-line no-console
+        console.log(error);
     }
-
-    const screenshot = await page.screenshot({ encoding: 'base64' });
-    await browser.close();
-
-    res.send({ blob: `data:image/jpeg;base64,${screenshot}` });
-    // will be passed to the page component as props
 }
