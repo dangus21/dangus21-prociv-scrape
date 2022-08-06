216f87bd8f89321228bc71d5d7bd4c7e1031c8e0
Miguel Magalhaes <ext.miguel.magalhaes@farfetch.com>
2022-08-06 02:31:00 +0100

    main: [] 😭

--- pages/api/scrape_prociv.ts
+++ pages/api/scrape_prociv.ts
@@ -1,20 +1,53 @@
-import puppeteer from 'puppeteer';
-import chrome from 'chrome-aws-lambda';
-// import chromium from 'chrome-aws-lambda';
+import chromium from 'chrome-aws-lambda';
+
+async function getBrowserInstance() {
+    const executablePath = await chromium.executablePath;
+
+    if (!executablePath) {
+        // running locally
+        // eslint-disable-next-line @typescript-eslint/no-var-requires
+        const puppeteer = require('puppeteer');
+        return puppeteer.launch({
+            args: chromium.args,
+            headless: true,
+            defaultViewport: {
+                width: 1280,
+                height: 720,
+            },
+            ignoreHTTPSErrors: true,
+        });
+    }
+
+    return chromium.puppeteer.launch({
+        args: chromium.args,
+        defaultViewport: {
+            width: 1280,
+            height: 720,
+        },
+        executablePath,
+        headless: chromium.headless,
+        ignoreHTTPSErrors: true,
+    });
+}
+
 export default async function scrape(req, res) {
+    let browser = null;
     try {
-        const browser = await puppeteer.launch(
-            process.env.NODE_ENV === 'production'
-                ? {
-                      args: chrome.args,
-                      executablePath: await chrome.executablePath,
-                      headless: chrome.headless,
-                  }
-                : { args: ['--no-sandbox', '--disable-setuid-sandbox'] }
-        );
+        // const browser = await puppeteer.launch(
+        //     process.env.NODE_ENV === 'production'
+        //         ? {
+        //               args: chromium.args,
+        //               executablePath: await chromium.executablePath,
+        //               headless: chromium.headless,
+        //           }
+        //         : { args: ['--no-sandbox', '--disable-setuid-sandbox'] }
+        // );
         // const browser = await puppeteer.launch();
 
+        browser = await getBrowserInstance();
         const page = await browser.newPage();
+
+        // const page = await browser.newPage();
 
         await page.setViewport({ width: 1920, height: 1080 });
 
@@ -82,10 +115,12 @@ export default async function scrape(req, res) {
             );
         }
 
-        const screenshot = await page.screenshot({ encoding: 'base64' });
+        const screenshot = await page.screenshot();
         await browser.close();
 
-        res.json({ blob: `data:image/jpeg;base64,${screenshot}` });
+        res.json({
+            blob: `data:image/jpeg;base64,${screenshot.toString('base64')}`,
+        });
     } catch (error) {
         // eslint-disable-next-line no-console
         console.log(error);
