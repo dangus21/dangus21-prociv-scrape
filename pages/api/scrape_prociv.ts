be38e1ba44ca6bec101dea77f7ad5ca384070e92
Miguel Magalhaes <ext.miguel.magalhaes@farfetch.com>
2022-08-07 21:39:24 +0100

    main: [] early returns

--- pages/api/scrape_prociv.ts
+++ pages/api/scrape_prociv.ts
@@ -3,92 +3,109 @@ import chrome from 'chrome-aws-lambda';
 import puppeteer from 'puppeteer-core';
 
 export default async function screenshot(req, res) {
-    const options = process.env.AWS_REGION
-        ? {
-              args: chrome.args,
-              executablePath: await chrome.executablePath,
-              headless: chrome.headless,
-              ignoreDefaultArgs: ['--disable-extensions'],
-          }
-        : {
-              args: [],
-              ignoreDefaultArgs: ['--disable-extensions'],
-              executablePath:
-                  process.platform === 'win32'
-                      ? 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe'
-                      : process.platform === 'linux'
-                      ? '/usr/bin/google-chrome'
-                      : '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
-          };
-
-    const browser = await puppeteer.launch(options);
-    const page = await browser.newPage();
-    await page.setViewport({ width: 1920, height: 1080 });
-    await page.goto(
-        'http://www.prociv.pt/pt-pt/SITUACAOOPERACIONAL/Paginas/default.aspx?cID=11'
-    );
-
+    let page: puppeteer.Page;
+    let browser: puppeteer.Browser;
     try {
-        await page.waitForFunction(
-            () =>
-                (
-                    document.querySelector(
-                        '#listOcorrenciasDetails tbody'
-                    ) as HTMLElement
-                ).childElementCount || 0 > 0
+        const options = process.env.AWS_REGION
+            ? {
+                  args: chrome.args,
+                  executablePath: await chrome.executablePath,
+                  headless: chrome.headless,
+                  ignoreDefaultArgs: ['--disable-extensions'],
+              }
+            : {
+                  args: [],
+                  ignoreDefaultArgs: ['--disable-extensions'],
+                  executablePath:
+                      process.platform === 'win32'
+                          ? 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe'
+                          : process.platform === 'linux'
+                          ? '/usr/bin/google-chrome'
+                          : '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+              };
+
+        browser = await puppeteer.launch(options);
+        page = await browser.newPage();
+        await page.setViewport({ width: 1920, height: 1080 });
+        await page.goto(
+            'http://www.prociv.pt/pt-pt/SITUACAOOPERACIONAL/Paginas/default.aspx?cID=11'
         );
-    } catch (error) {
-        // eslint-disable-next-line no-console
-        console.log(error);
-    }
 
-    await page.evaluate(() => {
-        const element = document.querySelector('#listOcorrenciasDetails tbody');
-        if (element) {
-            element.scrollIntoView();
+        try {
+            await page.waitForFunction(
+                () =>
+                    (
+                        document.querySelector(
+                            '#listOcorrenciasDetails tbody'
+                        ) as HTMLElement
+                    ).childElementCount || 0 > 0
+            );
+        } catch (error) {
+            const screenshot = await page.screenshot({ encoding: 'base64' });
+            await browser.close();
+            return res.send({ blob: `data:image/jpeg;base64,${screenshot}` });
         }
-    });
 
-    const isElementVisible = async (page, cssSelector: string) => {
-        let visible = true;
-        await page
-            .waitForSelector(cssSelector, { visible: true, timeout: 2000 })
-            .catch(() => {
-                visible = false;
-            });
-        return visible;
-    };
+        await page.evaluate(() => {
+            const element = document.querySelector(
+                '#listOcorrenciasDetails tbody'
+            );
+            if (element) {
+                element.scrollIntoView();
+            }
+        });
 
-    let loadMoreVisible = await isElementVisible(
-        page,
-        '#listOcorrenciasDetails > table > tfoot > tr > th > span'
-    );
-    while (loadMoreVisible) {
-        loadMoreVisible = await isElementVisible(
+        const isElementVisible = async (page, cssSelector: string) => {
+            let visible = true;
+            await page
+                .waitForSelector(cssSelector, { visible: true, timeout: 2000 })
+                .catch(() => {
+                    visible = false;
+                });
+            return visible;
+        };
+
+        let loadMoreVisible = await isElementVisible(
             page,
             '#listOcorrenciasDetails > table > tfoot > tr > th > span'
         );
-        if (loadMoreVisible) {
-            await page
-                .click(
-                    '#listOcorrenciasDetails > table > tfoot > tr > th > span'
-                )
-                .catch((e) => {
-                    // eslint-disable-next-line no-console
-                    console.log(e);
-                });
-            await page.evaluate(() => {
-                const element = document.querySelector(
+        try {
+            while (loadMoreVisible) {
+                loadMoreVisible = await isElementVisible(
+                    page,
                     '#listOcorrenciasDetails > table > tfoot > tr > th > span'
                 );
-                if (element) {
-                    element.scrollIntoView();
+                if (
+                    loadMoreVisible &&
+                    page.$(
+                        '#listOcorrenciasDetails > table > tfoot > tr > th > span'
+                    ) !== null
+                ) {
+                    await page.click(
+                        '#listOcorrenciasDetails > table > tfoot > tr > th > span'
+                    );
+                    await page.evaluate(() => {
+                        const element = document.querySelector(
+                            '#listOcorrenciasDetails > table > tfoot > tr > th > span'
+                        );
+                        if (element) {
+                            element.scrollIntoView();
+                        }
+                    });
                 }
-            });
+            }
+        } catch (error) {
+            const screenshot = await page.screenshot({ encoding: 'base64' });
+            await browser.close();
+            return res.send({ blob: `data:image/jpeg;base64,${screenshot}` });
         }
-    }
-    const screenshot = await page.screenshot({ encoding: 'base64' });
 
-    await browser.close();
-    return res.send({ blob: `data:image/jpeg;base64,${screenshot}` });
+        const screenshot = await page.screenshot({ encoding: 'base64' });
+        await browser.close();
+        return res.send({ blob: `data:image/jpeg;base64,${screenshot}` });
+    } catch {
+        const screenshot = await page.screenshot({ encoding: 'base64' });
+        await browser.close();
+        return res.send({ blob: `data:image/jpeg;base64,${screenshot}` });
+    }
 }
